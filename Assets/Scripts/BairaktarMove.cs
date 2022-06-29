2b749b54e81391287caa298a4e12e5b6d5ff3875
Obi9Kenobi <stepan.rachynskyy@gmail.com>
2022-06-29 22:48:59 +0300

    Correcting bairaktar speed change system

--- Assets/Scripts/BairaktarMove.cs
+++ Assets/Scripts/BairaktarMove.cs
@@ -4,28 +4,36 @@ using UnityEngine;
 
 public class BairaktarMove : MonoBehaviour
 {
-    [SerializeField] float SpeedChange = 0.5f;
-    [SerializeField] float RotatingSpeed = 500f;
-    float Speed = 0;
-    bool Rotated = false;
-    float R = 0;
-    float Rotate = 0;
+    float SpeedChange = 0;
+    float RotatingSpeed = 50;
+    float Speed = 0.5f;
+    float MaxSpeed = 0.8f;
+    float MinSpeed = 0.2f;
 
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.visible = false;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Rotate = -Input.GetAxis("Horizontal") * Time.deltaTime * RotatingSpeed;
-        SpeedChange = Input.GetAxis("Vertocal") * Time.deltaTime * 1;
+        float R = -Input.GetAxis("Horizontal") * Time.deltaTime * RotatingSpeed;
+        SpeedChange = Input.GetAxis("Vertical") * Time.deltaTime * 0.3f;
         Speed += SpeedChange;
+        if (Speed < MinSpeed)
+        {
+            Speed = MinSpeed;
+        }
+        if (Speed > MaxSpeed)
+        {
+            Speed = MaxSpeed;
+        }
+        Debug.Log(Speed);
         float UD = -Input.GetAxis("Mouse Y") * Time.deltaTime * 150;
         float LR = Input.GetAxis("Mouse X") * Time.deltaTime * 150;
         transform.Translate(0, 0, Speed);
-        transform.Rotate(UD, LR, Rotate);
+        transform.Rotate(UD, LR, R);
     }
 }
